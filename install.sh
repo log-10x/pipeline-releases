f7cefaa9b5df5cc54fe0d5d30580aae7167b04b1
Dor Levi <dor.levi.dor@gmail.com>
2024-09-01 12:34:11 +0300

    Update install script to include native and config

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@ set -e
 GITHUB_REPO="log-10x/pipeline-releases"
 VERSION="0.4.1"
 FLAVOR="cloud"
+DOWNLOAD_CONFIG="true"
 
 # Argument parsing
 while [[ "$#" -gt 0 ]]; do
     case $1 in
+    	--no-config)
+			DOWNLOAD_CONFIG="false"
+			;;
         --version)
             VERSION="$2"
             shift
@@ -16,19 +20,19 @@ while [[ "$#" -gt 0 ]]; do
         --flavor)
             FLAVOR=$(echo "$2" | tr '[:upper:]' '[:lower:]')  # Convert to lowercase
 
-            if [[ "$FLAVOR" != "edge" && "$FLAVOR" != "cloud" ]]; then
-                echo "Invalid flavor: $FLAVOR. Allowed values are 'edge' / 'cloud'"
+            if [[ "$FLAVOR" != "edge" && "$FLAVOR" != "cloud" && "$FLAVOR" != "native" ]]; then
+                echo "Invalid flavor: $FLAVOR. Allowed values are 'edge' / 'cloud' / 'native'."
                 exit 1
             fi
             shift
             ;;
         --help)
-            echo "Usage: install.sh [--version <version>] [--flavor <edge|cloud>]"
+            echo "Usage: install.sh [--version <version>] [--flavor <edge|cloud|native>] [--no-config]"
             exit 0
             ;;
         *)
             echo "Unknown option: $1"
-            echo "Usage: install.sh [--version <version>] [--flavor <edge|cloud>]"
+            echo "Usage: install.sh [--version <version>] [--flavor <edge|cloud|native>] [--no-config]"
             exit 1
             ;;
     esac
@@ -48,34 +52,36 @@ else
     exit 1
 fi
 
+#Validate native on supported os only
+if [ "$FLAVOR" == "native" ]; then
+    if [[ "$OS" != "ubuntu" && "$OS" != "debian" ]]; then
+        echo "Unsupported operating system $OS for $FLAVOR"
+        exit 1
+    fi
+fi
+
+ARTIFACT_FILE=""
+CONFIG_FILE="log10x-config-$L1X_VERSION.tar.gz"
 INSTALL_CMD=""
 
-# Create a temporary directory for the download
-TEMP_DIR=$(mktemp -d)
-
 # Set commands based on OS and flavor
-if [[ "$OS" == "ubuntu" || "$OS" == "debian" ]]; then
-    PACKAGE_FILE="${L1X_FLAVOR}_$L1X_VERSION-1_amd64.deb"
-    PACKAGE_URL="https://github.com/$GITHUB_REPO/releases/download/$L1X_VERSION/$PACKAGE_FILE"
+if [ "$FLAVOR" == "native" ]; then
+    ARTIFACT_FILE="log10x-edge-$L1X_VERSION-native"
 
-    CURL_CMD="curl -f -L -o $TEMP_DIR/$PACKAGE_FILE $PACKAGE_URL"
-    
-    INSTALL_CMD="apt-get install -y $TEMP_DIR/$PACKAGE_FILE"
+elif [[ "$OS" == "ubuntu" || "$OS" == "debian" ]]; then
+    ARTIFACT_FILE="${L1X_FLAVOR}_$L1X_VERSION-1_amd64.deb"
+    INSTALL_CMD="apt-get install -y"
 
 elif [[ "$OS" == "centos" || "$OS" == "fedora" || "$OS" == "rhel" ]]; then
-    PACKAGE_FILE="$L1X_FLAVOR-$L1X_VERSION-1.x86_64.rpm"
-    PACKAGE_URL="https://github.com/$GITHUB_REPO/releases/download/$L1X_VERSION/$PACKAGE_FILE"
-
-    CURL_CMD="curl -f -L -o $TEMP_DIR/$PACKAGE_FILE $PACKAGE_URL"
-    
+    ARTIFACT_FILE="$L1X_FLAVOR-$L1X_VERSION-1.x86_64.rpm"
+        
     if [ command -v dnf &> /dev/null ]; then
-        INSTALL_CMD="dnf install -y $TEMP_DIR/$PACKAGE_FILE"
+        INSTALL_CMD="dnf install -y"
     else
-        INSTALL_CMD="yum install -y $TEMP_DIR/$PACKAGE_FILE"
+        INSTALL_CMD="yum install -y"
     fi
 else
     echo "Unsupported operating system: $OS"
-    rm -rf $TEMP_DIR
 
     exit 1
 fi
@@ -128,29 +134,85 @@ if ! command -v sudo > /dev/null; then
     exit 1
 fi
 
-echo "Going to download with: $CURL_CMD"
+ARTIFACT_URL="https://github.com/$GITHUB_REPO/releases/download/$L1X_VERSION/$ARTIFACT_FILE"
+
+# Create a temporary directory for the download
+TEMP_DIR=$(mktemp -d)
+
+CURL_CMD="curl -f -L -o $TEMP_DIR/$ARTIFACT_FILE $ARTIFACT_URL"
+
+echo ""
+echo "Downloading artifact: $CURL_CMD"
 $CURL_CMD
 
 if [ -n "$INSTALL_CMD" ]; then
-    echo "Going to install with: $INSTALL_CMD"
-    $INSTALL_CMD
+	echo ""
+    echo "Installing artifact with: $INSTALL_CMD $TEMP_DIR/$ARTIFACT_FILE"
+    echo ""
+    $INSTALL_CMD $TEMP_DIR/$ARTIFACT_FILE
+
+elif [ "$FLAVOR" == "native" ]; then
+	echo ""
+	echo "Installing native artifact..."
+
+	L1X_FLAVOR="log10x-edge"
+
+    mkdir -p "/opt/$L1X_FLAVOR/bin"
+    mv "$TEMP_DIR/$ARTIFACT_FILE" "/opt/$L1X_FLAVOR/bin/$ARTIFACT_FILE"
+    chmod +x "/opt/$L1X_FLAVOR/bin/$ARTIFACT_FILE"
+    ln -s "/opt/$L1X_FLAVOR/bin/$ARTIFACT_FILE" "/opt/$L1X_FLAVOR/bin/$L1X_FLAVOR"
+fi
+
+ln -s "/opt/$L1X_FLAVOR/bin/$L1X_FLAVOR" "/opt/$L1X_FLAVOR/bin/log10x"
+
+L1X_PATH="/etc/log10x/config"
+
+if [ "$DOWNLOAD_CONFIG" == "true" ]; then
+	CONFIG_URL="https://github.com/$GITHUB_REPO/releases/download/$L1X_VERSION/$CONFIG_FILE"
+	CONFIG_CURL="curl -f -L -o $TEMP_DIR/$CONFIG_FILE $CONFIG_URL"
+
+	echo ""
+	echo "Downloading Log10x configuration: $CONFIG_CURL"
+	$CONFIG_CURL
+
+	echo ""
+	echo "Unpacking Log10x configuration into $L1X_PATH"
+
+	mkdir -p "$L1X_PATH"
+	tar -xzf "$TEMP_DIR/$CONFIG_FILE" -C "$L1X_PATH"
 fi
 
 # Set up the environment variable
+echo ""
+echo "Setting up environment variables"
 echo "export L1X_HOME=/opt/$L1X_FLAVOR" | sudo tee "/etc/profile.d/$L1X_FLAVOR.sh"
 echo "export L1X_BIN=\$L1X_HOME/bin/$L1X_FLAVOR" | sudo tee -a "/etc/profile.d/$L1X_FLAVOR.sh"
 echo "export PATH=\$L1X_HOME/bin:\$PATH" | sudo tee -a "/etc/profile.d/$L1X_FLAVOR.sh"
 
+if [ "$DOWNLOAD_CONFIG" == "true" ]; then
+echo "export L1X_PATH=$L1X_PATH" | sudo tee -a "/etc/profile.d/$L1X_FLAVOR.sh"
+fi
+
 # Clean up
 rm -rf $TEMP_DIR
 
+echo ""
 echo "Installation complete."
 echo ""
-echo "Installed into L1X_HOME - /opt/$L1X_FLAVOR"
+echo "Installed into - /opt/$L1X_FLAVOR"
 echo ""
-echo "Set L1X_BIN as - /opt/$L1X_FLAVOR/bin/$L1X_FLAVOR"
+echo "Configured the following environment variables:"
+echo "    L1X_HOME - /opt/$L1X_FLAVOR"
+echo "    L1X_BIN -  /opt/$L1X_FLAVOR/bin/$L1X_FLAVOR"
+
+if [ "$DOWNLOAD_CONFIG" == "true" ]; then
+echo "    L1X_PATH - $L1X_PATH"
+fi
+
 echo ""
 echo "Added bin - /opt/$L1X_FLAVOR/bin - to \$PATH"
+echo ""
+echo "Log10x log file is written into /var/log/l1x/"
 echo ""
 echo "Please restart your terminal or run 'source /etc/profile.d/$L1X_FLAVOR.sh' to apply the environment variables."
 echo ""
